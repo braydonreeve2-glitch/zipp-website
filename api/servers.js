db2c629dd9779c601bdc408c0db982d9d581e589
braydonreeve2-glitch <braydonreeve2@gmail.com>
2026-02-10 14:04:41 -0500

    Simplify server response to return a static value

--- api/servers.js
+++ api/servers.js
@@ -1,17 +1,3 @@
 export default async function handler(req, res) {
-  try {
-    const response = await fetch(process.env.BOT_API_URL, {
-      headers: {
-        Authorization: `Bearer ${process.env.BOT_API_KEY}`
-      }
-    });
-
-    if (!response.ok) throw new Error('Bot API error');
-
-    const data = await response.json();
-
-    res.status(200).json({ servers: data.servers });
-  } catch (err) {
-    res.status(200).json({ servers: 0 });
-  }
+  res.status(200).json({ servers: 123 });
 }
